1c76aa8e66efdb76aed4c3f095fec47e7b7b3dd0
Tomas Roun <tomas.roun@cern.ch>
2023-03-10 14:13:14 +0100

    Update CI

--- test.sh
+++ test.sh
@@ -1,46 +1,40 @@
 #!/bin/bash
 
-# cp indico-web.env.sample indico-web.env
-# sed ...
-
-URL=http://localhost:9090/category/0/statistics
+DIR="indico-${1:-prod}"
+URL=http://localhost:8080/category/0/statistics
 TIMEOUT=120
 
-# make sure the cluster is down
-docker-compose down
-# then try to bring it up
-docker-compose up -d
-
-timeout_handler() {
-    # SIGALRM caught, let's just exit
-    exit 1
+check_indico_status() {
+    response=$(curl -L --write-out '%{http_code}' --silent --output /dev/null $URL)
+    [[ $response = "200" ]]
 }
 
-trap timeout_handler ALRM
+cd $DIR
+# make sure the cluster is down
+docker compose down
+# then try to bring it up
+docker compose up &
 
-# this is the timer process that will send SIGALRM
-pid=$$
-(
-    sleep $TIMEOUT
-    echo 'Timeout! Killing process'
-    kill -s ALRM $pid
-) &
-alarm=$!
+start_time="$(date -u +%s)"
+until check_indico_status; do
+    echo "Waiting for Indico to become available..."
+    sleep 10
 
-while [[ "$(curl -L --max-time 10 -s -o /dev/null -w ''%{http_code}'' $URL)" != "200" ]]; do
-    sleep 30;
-    echo 'Waiting...'
+    curr_time="$(date -u +%s)"
+    if ((($curr_time - $start_time) > TIMEOUT)); then
+        break
+    fi
 done
 
 # Print response from server, for clarity
-curl -L $URL
+echo "Response from Indico:"
+curl -L --silent $URL
+echo ""
 
 # yay!
-echo 'Indico seems alive!'
+echo "Indico seems alive!"
 
-# remove timer
-kill -s TERM $alarm > /dev/null 2>&1
-
-docker-compose down
+echo "Shutting down..."
+docker compose down
 
 exit 0
